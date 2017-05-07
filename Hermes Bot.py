071c441833824312a0a1e58543999962ed68a32e
Ghostpowerz <ghostpowerz@gmail.com>
2017-05-07 01:44:04 +0100

    Update Hermes Bot.py

--- Hermes Bot.py
+++ Hermes Bot.py
@@ -1,15 +1,30 @@
 import discord
-from discord.ext.commands import bot
+import logging
 
-Hermes_Bot =bot(command_prefix="!")
+
+from discord.ext.commands import Bot
+
+
+def log():
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger('discord')
+    handler = logging.FileHandler(filename='discord.log',encoding='utf-8',mode='w')
+    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+    logger.addHandler(handler)
+log()
+
+Hermes_Bot = Bot(command_prefix="!")
+
 
 @Hermes_Bot.event
-async def on_read():
-    print ("Blast off")
+async def on_ready():
+    print("Blast off")
 
-@Hermes_Bot.command()
-async def hello (*args):
-    return await Hermes_Bot.say ("Alright cunts")
 
-Hermes_Bot.run("{MzEwNTIwMzcwMzc5NjIwMzUz.C-_K7g.wc3TaZqSVOZ0zjyIns33y2jQaCM}")
+def commands():
+    @Hermes_Bot.command()
+    async def hello(*args):
+        return await Hermes_Bot.say ("Alright cunts")
+
+Hermes_Bot.run("MzEwNTIwMzcwMzc5NjIwMzUz.C-_4GA.Kupqd38QG9s7C-FL41WZ59igJKQ")
 
