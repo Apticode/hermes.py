97b7d3853b0dc52fc1e4b8dab2f2d0b709466439
Ghostpowerz <ghostpowerz@gmail.com>
2017-05-08 02:01:19 +0100

    Functional Hermes.py

--- hermes.py
+++ hermes.py
@@ -2,7 +2,8 @@ import discord
 import logging
 import asyncio
 import os
-
+import sys
+import time
 
 from discord.ext import commands
 
@@ -10,46 +11,48 @@ from discord.ext import commands
 def log():
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger('discord')
-    handler = logging.FileHandler(filename='discord.log',encoding='utf-8',mode='w')
+    handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
     handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
     logger.addHandler(handler)
 log()
 
 description = 'Hello you cunts.'
 
-Hermes_Bot = commands.Bot(command_prefix='?', description=description)
+hermes = commands.Bot(command_prefix='?', description=description)
+
 
 client = discord.Client()
-member = discord.Member
+
 
 def event():
-    @Hermes_Bot.event
-
-    async def on_ready():
-        print('Logged in as:\n{0} (ID: {0.id})'.format(Hermes_Bot.user))
-        await Hermes_Bot.change_presence(game=discord.Game(name="Futurama"))
-
+    @client.event
     async def on_member_join(member):
         server = member.server
-        fmt = 'Welcome {0.mention} to {1.name}!'
-        await client.send_message(server, fmt.format(member, server))
+        welcome_message = 'Welcome {0.mention} to {1.name}!'
+        await client.send_message(server, welcome_message.format(member, server))
 
+    @hermes.event
+    async def on_ready():
+        print('Logged in as:\n{0} (ID: {0.id})'.format(hermes.user))
+        await hermes.change_presence(game=discord.Game(name="Futurama"))\
 
 
 event()
 
 
-def commands():
-    @Hermes_Bot.command()
+def hermes_command():
+    @hermes.command()
     async def hello():
-        return await Hermes_Bot.say ("Alright cunts")
+        await hermes.say("WAGWAN")
 
-    async def joined(member: discord.member):
-        """Fatty has arrived"""
-        await Hermes_Bot.say('{0.name} joined in {0.joined_at}'.format(member))
+    @hermes.command()
+    async def info():
+        return await hermes.say("This bot was produced in collaboration between darkhunters and jenk "
+                                " \n For further information please private message one of the "
+                                "developers or moderators available")
+
+hermes_command()
 
 
-commands()
+hermes.run("MzEwNTIwMzcwMzc5NjIwMzUz.C-_4GA.Kupqd38QG9s7C-FL41WZ59igJKQ")
 
-
-Hermes_Bot.run("MzEwNTIwMzcwMzc5NjIwMzUz.C-_4GA.Kupqd38QG9s7C-FL41WZ59igJKQ")
