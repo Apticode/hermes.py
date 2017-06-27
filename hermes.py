460c6297ea287760da137762490623e7cf67015c
Ghostpowerz <ghostpowerz@gmail.com>
2017-06-27 20:16:43 +0100

    Updates Hermes to use Arrays document appropriately

--- hermes.py
+++ hermes.py
@@ -6,7 +6,7 @@ import sys
 import time
 
 from discord.ext import commands
-
+from Arrays import *
 
 def log():
     logging.basicConfig(level=logging.INFO)
@@ -22,45 +22,6 @@ current_time = (time.strftime("%H:%M:%S"))
 
 current_date = (time.strftime("%d/%m/%Y"))
 
-ball_answers = [
-    "The answer lies in your heart.",
-    "I do not know.",
-    "Almost certainly.",
-    "No.",
-    "Yes.",
-    "Why do you need to ask?",
-    "Go away. I do not wish to answer at this time.",
-    "Time will only tell.",
-]
-
-rate_answers = [
-    "I rate you 1/10.",
-    "I rate you 2/10.",
-    "I rate you 3/10.",
-    "I rate you 4/10.",
-    "I rate you 5/10.",
-    "I rate you 6/10.",
-    "I rate you 7/10.",
-    "I rate you 8/10.",
-    "I rate you 9/10.",
-    "I rate you 10/10.",
-]
-
-coin = [
-    "Heads.",
-    "Tails.",
-]
-
-quotes = [
-    "Hermes: \"Sweet something...of...someplace..\"",
-    "Hermes: \"Sweet giant anteater of Santa Anita!\"",
-    "Hermes: \"Sweet llamas of the Bahamas!\"",
-    "Hermes: \"You're just a giant lump of fat. Do you even have an ass under there?\"",
-    "Hermes: \"On the bright side, I'll never see Zoidberg again.\"",
-    "Hermes: \"It was Zoidberg!\"",
-    "Hermes: \"Come on, woman! Just pick something.\"",
-    "Hermes: \"Oh, Bender's a model employee.\"",
-]
 
 hermes = commands.Bot(command_prefix='?', description=description)
 
