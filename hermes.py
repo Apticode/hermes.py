b7098c538637809417b58401fa9cf9db14ca08e1
Ghostpowerz <ghostpowerz@gmail.com>
2017-07-07 03:34:49 +0100

    Updated hermes for cogs

--- hermes.py
+++ hermes.py
@@ -1,32 +1,5 @@
-import discord
-import logging
-import asyncio
-import random
-import sys
-import time
-
-from discord.ext import commands
-from Arrays import *
-
-def log():
-    logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger('discord')
-    handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-    logger.addHandler(handler)
-log()
-
-description = 'As fast as the Greek God and as flexible as the Jamaican.'
-
-current_time = (time.strftime("%H:%M:%S"))
-
-current_date = (time.strftime("%d/%m/%Y"))
-
-
-hermes = commands.Bot(command_prefix='?', description=description)
-
-
-client = discord.Client()
+from core_variables import *
+from log_function import *
 
 
 def event():
@@ -38,70 +11,39 @@ def event():
 
     @hermes.event
     async def on_ready():
+        print("------------------------")
         print('Logged in as:\n{0} (ID: {0.id})'.format(hermes.user))
+        print("------------------------")
         await hermes.change_presence(game=discord.Game(name="Futurama"))\
 
 
+
+@hermes.command()
+async def load(extension_name : str):
+    try:
+        hermes.load_extension(extension_name)
+    except (AttributeError, ImportError) as h:
+        await hermes.say("```py\n{}: {}\n```".format(type(h).__name__, str(h)))
+        return
+    await hermes.say("{} loaded.".format(extension_name))
+
+
+@hermes.command()
+async def unload(extension_name : str):
+    hermes.unload_extension(extension_name)
+    await hermes.say("{} unloaded.".format(extension_name))
+
+
+if __name__ == "__main__":
+
+    for extension in modular_initiation:
+        try:
+            hermes.load_extension(extension)
+        except Exception as h:
+            exc = '{}: {}'.format(type(h).__name__, h)
+            print('Failed to load module {}\n{}'.format(extension, exc))
+
+
 event()
-
-
-def hermes_command():
-    @hermes.command()
-    async def hello():
-        await hermes.say("WAGWAN")
-
-    @hermes.command()
-    async def bye():
-        await hermes.say("Safe bruv!")
-
-    @hermes.command()
-    async def website():
-        await hermes.say("You can find our official website @ https://hermespy.tk/")
-
-    @hermes.command()
-    async def info():
-        return await hermes.say("This bot was produced in collaboration between darkhunters and jenk "
-                                " \n For further information please private message one of the "
-                                "developers or moderators available")
-
-    @hermes.command()
-    async def time():
-        await hermes.say(current_time)
-
-    @hermes.command(name="commands")
-    async def command_list():
-        await hermes.say("```Here is a list of some of the commands you can use by using ? then the command\n"
-                         "1:) commands \n"
-                         "2:) hello \n"
-                         "3:) info \n"
-                         "4:) time \n"
-                         "5:) date \n"
-                         "6:) 8ball \n"
-                         "7:) coinflip \n"
-                         "8:) quote \n```"
-                         )
-
-    @hermes.command()
-    async def date():
-        await hermes.say(current_date)
-
-    @hermes.command(name="8ball")
-    async def ball():
-        await hermes.say(random.choice(ball_answers))
-
-    @hermes.command()
-    async def coinflip():
-        await hermes.say(random.choice(coin))
-
-    @hermes.command()
-    async def rateme():
-        await hermes.say(random.choice(rate_answers))
-
-    @hermes.command(name="quote")
-    async def randomquote():
-        await hermes.say(random.choice(quotes))
-
-hermes_command()
-
-
-hermes.run("TOKEN-HERE")
+log()
+hermes.run("Token here")
