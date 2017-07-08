d5bc69c01c5fdd1438649d5a4232ba75ee8ac6ec
Ghostpowerz <ghostpowerz@gmail.com>
2017-07-08 18:45:01 +0100

    Delete log_function.py

--- Other/log_function.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-
-def log():
-    logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger('discord')
-    handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-    logger.addHandler(handler)
