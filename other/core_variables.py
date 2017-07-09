64a74f3ae638d30a00bbd1d9421c013ad5023de0
Ghostpowerz <ghostpowerz@gmail.com>
2017-07-09 20:19:46 +0100

    Update core_variables.py

--- other/core_variables.py
+++ other/core_variables.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 
 description = 'As fast as the Greek God and as flexible as the Jamaican.'
 
-modular_initiation = ["rng", "information","time_and_date", "trivia"]
+modular_initiation = ["cogs.rng", "cogs.information","cogs.time_and_date", "cogs.trivia"]
 
 hermes = commands.Bot(command_prefix='?', description=description)
 
