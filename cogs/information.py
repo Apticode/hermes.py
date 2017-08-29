ed8b41e28873321f20c33ad860cec66fd1f3b17c
Ghostpowerz <Ghostpowerz@users.noreply.github.com>
2017-08-29 01:35:49 +0100

    Update information.py

--- cogs/information.py
+++ cogs/information.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-
+import time
 
 class Information():
     def __init__(self, hermes):
@@ -7,20 +7,8 @@ class Information():
 
     @commands.command()
     async def website(self):
-        await self.hermes.say("You can find our official website @ https://hermespy.tk/")
+        await self.hermes.say("You can find our official website @ https://hermes.apticode.co.uk")
 
-    @commands.command()
-    async def helpme(self,):
-        await self.hermes.say("```Here is a list of some of the commands you can use by using ? then the command\n"
-                              "1:) commands \n"
-                              "2:) hello \n"
-                              "3:) info \n"
-                              "4:) time \n"
-                              "5:) date \n"
-                              "6:) 8ball \n"
-                              "7:) coinflip \n"
-                              "8:) quote \n```"
-                              )
 
     @commands.command()
     async def info(self):
@@ -28,6 +16,7 @@ class Information():
                                      " \n For further information please contact us on our discord @ "
                                      "https://discord.gg/qvNDzZg/ or visit our website, use the ?website command")
     @commands.command(pass_context=True)
+
     # makeshift ping for now
     async def ping(self,ctx):
         """pseudo-ping time"""
