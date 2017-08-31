297ff334b0a1744b34da5cd4e60e7084c3aba768
Ghostpowerz <Ghostpowerz@users.noreply.github.com>
2017-08-31 19:16:25 +0100

    Update checks.py

--- cogs/utils/checks.py
+++ cogs/utils/checks.py
@@ -1,8 +1,9 @@
 import discord.utils
 from discord.ext import commands
 
-# Credits for this goes to tuxbot which can be found at:
+# Credits for this goes to tuxbot and red bot which can be found at:
 # https://github.com/outout14/tuxbot-bot
+# https://github.com/Cog-Creators/Red-DiscordBot
 
 def is_owner_check(message):
     owner = message.author.id in ['216666694582534145', '184361521558716418']
@@ -19,43 +20,27 @@ def is_owner(warn=True):
     owner = commands.check(lambda ctx: check(ctx, warn))
     return owner
 
-
 def check_permissions(ctx, perms):
-    msg = ctx.message
-    if is_owner_check(msg):
+    if is_owner_check(ctx):
         return True
+    elif not perms:
+        return False
 
-    ch = msg.channel
-    author = msg.author
+    ch = ctx.message.channel
+    author = ctx.message.author
     resolved = ch.permissions_for(author)
     return all(getattr(resolved, name, None) == value for name, value in perms.items())
 
 
-def role_or_permissions(ctx, check, **perms):
-    if check_permissions(ctx, perms):
-        return True
-
-    ch = ctx.message.channel
-    author = ctx.message.author
-    if ch.is_private:
-        return False  # can't have roles in PMs
-
-    role = discord.utils.find(check, author.roles)
-    return role is not None
-
-
-def admin_or_permissions(**perms):
+def serverowner_or_permissions(**perms):
     def predicate(ctx):
-        return role_or_permissions(ctx, lambda r: r.name == 'Bot Admin', **perms)
-
-    return commands.check(predicate)
-
-
-def is_in_servers(*server_ids):
-    def predicate(ctx):
-        server = ctx.message.server
-        if server is None:
+        if ctx.message.server is None:
             return False
-        return server.id in server_ids
+        server = ctx.message.server
+        owner = server.owner
 
+        if ctx.message.author.id == owner.id:
+            return True
+
+        return check_permissions(ctx,perms)
     return commands.check(predicate)
