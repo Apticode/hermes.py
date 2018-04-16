48c0ed9e1e82bc08d49db288cad5589a9270229d
Ghostpowerz <Ghostpowerz@users.noreply.github.com>
2018-04-16 23:09:53 +0100

    Update trivia.py

--- cogs/trivia.py
+++ cogs/trivia.py
@@ -1,18 +1,31 @@
 from discord.ext import commands
+import time
 
-
-class Trivia():
+class Information():
     def __init__(self, hermes):
         self.hermes = hermes
 
     @commands.command()
-    async def hello(self):
-        await self.hermes.say("WAGWAN")
+    async def website(self):
+        await self.hermes.say("You can find our official website @ https://hermespy.apticode.co.uk")
+
 
     @commands.command()
-    async def trivia(self):
-        await self.hermes.say("byeee")
+    async def info(self):
+        return await self.hermes.say("This hermes was produced in collaboration between <@216666694582534145> and <@184361521558716418> "
+                                     " \n For further information please contact us on our discord @ "
+                                     "https://discord.gg/qvNDzZg/ or visit our website, use the ?website command")
+    @commands.command(pass_context=True)
+
+    # makeshift ping for now
+    async def ping(self,ctx):
+        """pseudo-ping time"""
+        channel = ctx.message.channel
+        t1 = time.perf_counter()
+        await self.hermes.send_typing(channel)
+        t2 = time.perf_counter()
+        await self.hermes.say("ping: {}ms".format(round((t2-t1)*1000)))
 
 
 def setup(hermes):
-    hermes.add_cog(Trivia(hermes))
+    hermes.add_cog(Information(hermes))
