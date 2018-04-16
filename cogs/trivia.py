a7a2c2f65b16dde83282b72a245918b22b70558a
Ghostpowerz <Ghostpowerz@users.noreply.github.com>
2018-04-16 23:51:10 +0100

    Update trivia.py

--- cogs/trivia.py
+++ cogs/trivia.py
@@ -1,31 +1,31 @@
 from discord.ext import commands
 import time
 
-class Information():
+class Trivia:
     def __init__(self, hermes):
         self.hermes = hermes
 
     @commands.command()
-    async def website(self):
-        await self.hermes.say("You can find our official website @ https://hermespy.apticode.co.uk")
-
+    async def hello(self, ctx):
+        await ctx.send('Hey' + str(ctx.author))
 
     @commands.command()
-    async def info(self):
-        return await self.hermes.say("This hermes was produced in collaboration between <@216666694582534145> and <@184361521558716418> "
-                                     " \n For further information please contact us on our discord @ "
-                                     "https://discord.gg/qvNDzZg/ or visit our website, use the ?website command")
-    @commands.command(pass_context=True)
+    async def trivia(self):
+        await self.hermes.say("byeee")
+
+    @commands.command()
+    async def test(self, arg):
+        await self.hermes.say(arg)
 
     # makeshift ping for now
-    async def ping(self,ctx):
+    async def ping(self, ctx):
         """pseudo-ping time"""
         channel = ctx.message.channel
         t1 = time.perf_counter()
         await self.hermes.send_typing(channel)
         t2 = time.perf_counter()
-        await self.hermes.say("ping: {}ms".format(round((t2-t1)*1000)))
+        await self.hermes.say("ping: {}ms".format(round((t2 - t1) * 1000)))
 
 
 def setup(hermes):
-    hermes.add_cog(Information(hermes))
+    hermes.add_cog(Trivia(hermes))
